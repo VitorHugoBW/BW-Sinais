1eb6beebc2f42e8e95a23bbbf13210fa97fabb27
VitorHugoBW <143046037+VitorHugoBW@users.noreply.github.com>
2023-08-23 21:46:35 -0300

    primeiro upload

---
+++ bwsinais.py
@@ -0,0 +1,9 @@
+numero1 = 10
+numero2 = 15
+
+if numero1 > numero2:
+    print(F"O número {numero1} é maior que {numero2}!")
+elif numero1 == numero2:
+    print(F"{numero1} e {numero2} são números iguais!")
+elif numero1 < numero2:
+    print(F"O número {numero1} é menor que o númenor{numero2}")
